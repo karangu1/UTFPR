27896dcbcf687f1ca5c3f268413ab638d7e6c56b
Fernando Almeida <fernandoalmeida@alunos.utfpr.edu.br>
2019-10-26 21:24:00 -0300

    auto push

--- autopush.bat
+++ autopush.bat
@@ -2,7 +2,7 @@
 	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. C:\Users\pookie\Desktop\Writings
-	cd <path>
+	::cd <path>
 	
 	::Initialize GitHub
 	git init
